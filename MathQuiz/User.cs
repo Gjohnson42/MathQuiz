07b66af0362fda4bfc241e2f866aa9b2f5231ee4
Garrett Johnson <gjohnson49@liberty.edu>
2020-07-13 12:21:49 -0400

    Put in a user class, to experiment with combining functions from outside classes to forms.

---
+++ MathQuiz/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathQuiz
+{
+    class User
+    {
+        //Single argument constructor that takes in a name. 
+        User(string passedInUserName) 
+        {
+            UserName = passedInUserName;
+        }
+        private string userName = "John";
+        public string UserName
+        {
+            get;
+            set;
+        }
+        //This function returns a string using the user's name, sending it to the form. 
+        public string makeGreeting() 
+        {
+            string greeting = "Hello " + UserName;
+            return greeting;
+        }
+    }
+}
