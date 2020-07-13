2a15d0d00da153c6e03c8c76a904798887d37169
Garrett Johnson <gjohnson49@liberty.edu>
2020-07-13 12:12:30 -0400

    A basic startTheQuiz function is now in effect.

--- MathQuiz/Form1.cs
+++ MathQuiz/Form1.cs
@@ -12,9 +12,30 @@ namespace MathQuiz
 {
     public partial class Form1 : Form
     {
+        //Variable instantiation Section
+        //Random class instantiation used for developing RNG Values
+        Random randomizer = new Random();
+
+        //Addition Variables
+        const int MAX_ADDITION_VALUE = 50;
+        int addend1;
+        int addend2;
         public Form1()
         {
             InitializeComponent();
+        }
+        //Form Methods
+        public void startTheQuiz() 
+        {
+            //Sets up a value for the addition arguments, changes the labels, and resets the sum to zero.
+            addend1 = randomizer.Next(MAX_ADDITION_VALUE+1);
+            addend2 = randomizer.Next(MAX_ADDITION_VALUE + 1);
+
+            //Changing labels
+            plusLeftLabel.Text = addend1.ToString();
+            plusRightLabel.Text = addend2.ToString();
+            sumNumericUpDown.Value = 0;
+
         }
 
         private void plusSignLabel_Click(object sender, EventArgs e)
@@ -29,7 +50,8 @@ namespace MathQuiz
 
         private void quizStartBtn_Click(object sender, EventArgs e)
         {
-
+            startTheQuiz();
+            quizStartBtn.Enabled = false;
         }
     }
 }
